986272c68cf8ae6685b5d5ff8632b944adf47c2c
Owen Goodman <owengoodman3@vt.edu>
2025-11-18 10:01:57 -0500

    updated export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "out",
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
