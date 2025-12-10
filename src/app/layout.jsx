2f567ece491e8c5e355f1aa1237514651bff1b67
Owen Goodman <owengoodman3@gmail.com>
2025-12-10 14:35:42 -0500

    ico

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
